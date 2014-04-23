f4a151d3e20a849991ea2a0037277ce1ab22e34a
Oleksandr Orlov <oorlov@sdl.com>
2014-04-23 19:19:00 +0300

    Initial Check in of the solution
    Initial Check in of the solution

---
+++ Extension/ServiceProxy.Extension.js
@@ -0,0 +1,24 @@
+﻿Tridion.Type.registerNamespace("Sys.Net");
+
+if (Sys.Net.WebServiceProxy)
+{
+	/**
+	 * Hide implementation in local scope
+	 */
+	(function ()
+	{
+		//	Clearly identify the method you override, and an extension where you are doing so.
+		//	I`d suggest the following pattern "_overridden${original function name}${extension name}"
+		var _overridden$WebServiceProxy$onComplete$UIBeardcore = Sys.Net.WebServiceProxy.onComplete;
+		Sys.Net.WebServiceProxy.onComplete = function WebServiceProxy$onComplete$UIBeardcore(request)
+		{
+			console.log(request && request.url);
+
+			// Use apply(this, arguments) unless you don`t want to modify arguments
+			var result = _overridden$WebServiceProxy$onComplete$UIBeardcore.apply(this, arguments);
+
+			// Check an initial implementation, if execution result has to be returned, in our case we don`t have to.
+			//return result;
+		};
+	})();
+}
